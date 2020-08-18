db5a503c80f7b840a9dc9e8867dc32225916fe4d
Antonio Hernández Blas <1096022+nihilismus@users.noreply.github.com>
2020-08-17 20:52:08 -0500

    Test loading of occupied positions in Grid

--- lib/jdlv.rb
+++ lib/jdlv.rb
@@ -3,6 +3,7 @@
 require 'jdlv/version'
 require 'jdlv/position'
 require 'jdlv/cell'
+require 'jdlv/grid'
 
 module Jdlv
   class Error < StandardError; end
--- spec/jdlv/grid_spec.rb
+++ spec/jdlv/grid_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Jdlv::Grid do
     last_position = Jdlv::Position.new(8, 4)
     expect(described_class.new(last_position, []).area).to eq(32)
   end
+
+  it 'has 3 occupied cells' do
+    last_position = Jdlv::Position.new(4, 8)
+    positions = [Jdlv::Position.new(1, 1), Jdlv::Position.new(2, 2), Jdlv::Position.new(1, 3)]
+    expect(described_class.new(last_position, positions).occupied_cells).to eq(3)
+  end
 end
