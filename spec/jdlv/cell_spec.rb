256dd1c2e20df77d9b5d7748eff14a123f73c49a
Antonio Hernández Blas <1096022+nihilismus@users.noreply.github.com>
2020-08-17 21:27:24 -0500

    Amend offenses to rubocop

--- spec/jdlv/cell_spec.rb
+++ spec/jdlv/cell_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Jdlv::Cell do
   it 'is in a occupied current state' do
     expect(described_class.new(Jdlv::Position.new(4, 8)).current_state).to eq(:occupied)
   end
+
   it 'is in an unknown next state' do
     expect(described_class.new(Jdlv::Position.new(4, 8)).next_state).to eq(:unknown)
   end
--- spec/jdlv/grid_spec.rb
+++ spec/jdlv/grid_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Jdlv::Grid do
   it 'has an area of 32' do
     last_position = Jdlv::Position.new(4, 8)
     expect(described_class.new(last_position, []).area).to eq(32)
+  end
 
+  it 'has an area of 32 too' do
     last_position = Jdlv::Position.new(8, 4)
     expect(described_class.new(last_position, []).area).to eq(32)
   end
