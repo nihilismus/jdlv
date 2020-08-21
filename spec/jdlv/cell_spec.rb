790130754c47986a0191f9f8ddfcc9c6ac761a0f
Antonio Hernández Blas <1096022+nihilismus@users.noreply.github.com>
2020-08-21 14:40:06 -0500

    Update Cell spec

--- spec/jdlv/cell_spec.rb
+++ spec/jdlv/cell_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe Jdlv::Cell do
-  it 'is in a occupied current state' do
+  it 'is in a occupied current state by default' do
     expect(described_class.new(Jdlv::Position.new(4, 8)).current_state).to eq(:occupied)
   end
 
-  it 'is in an unknown next state' do
+  it 'is in an unknown next state by default' do
     expect(described_class.new(Jdlv::Position.new(4, 8)).next_state).to eq(:unknown)
   end
 end
