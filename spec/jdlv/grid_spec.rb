d825210d4963e0d50bb2da5ea172dab4ca89a415
Antonio Hernández Blas <1096022+nihilismus@users.noreply.github.com>
2020-08-21 14:42:38 -0500

    Update Grid spec

--- spec/jdlv/grid_spec.rb
+++ spec/jdlv/grid_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Jdlv::Grid do
     expect(described_class.new(position, []).area).to eq(32)
   end
 
-  it 'has 3 occupied cells' do
+  it 'has 3 occupied positions' do
     position = Jdlv::Position.new(4, 8)
     positions = [Jdlv::Position.new(1, 1), Jdlv::Position.new(2, 2), Jdlv::Position.new(1, 3)]
-    expect(described_class.new(position, positions).occupied_cells_count).to eq(3)
+    expect(described_class.new(position, positions).number_occupied_positions).to eq(3)
   end
 end
