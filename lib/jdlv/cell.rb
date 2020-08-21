795b39f40c4cf87ebc378db61d53c55615e616f9
Antonio Hernández Blas <1096022+nihilismus@users.noreply.github.com>
2020-08-21 14:39:49 -0500

    Add Cell's logic to respect tick events

--- lib/jdlv/cell.rb
+++ lib/jdlv/cell.rb
@@ -10,5 +10,22 @@ module Jdlv
       @current_state = :occupied
       @next_state = :unknown
     end
+
+    def review_state(grid)
+      apply_rules grid.number_positions_occupied_around(@position)
+    end
+
+    def update_state
+      @current_state = @next_state
+      @next_state = :unknown
+    end
+
+    private
+
+    def apply_rules(number_neighbours)
+      @next_state = :free if number_neighbours < 2
+      @next_state = :occupied if [2, 3].include? number_neighbours
+      @next_state = :free if number_neighbours > 3
+    end
   end
 end
